33ed726fa6a8bc6f2798fc4bd37602c8a93146ac
John Emad Morcos Sadek | Ejada Systems Ltd <jemad@ejadacairo.local>
2023-01-24 13:01:40 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FilterComponent } from './filter/filter.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { FormsModule } from '@angular/forms';
+import { MoviesComponent } from './movies/movies.component';
+import { FilterListComponent } from './filter-list/filter-list.component';
+import { ButtonBgDirective } from './custom directives/button-bg.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    FilterComponent,
+    HeaderComponent,
+    SearchComponent,
+    MoviesComponent,
+    FilterListComponent,
+    ButtonBgDirective,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
